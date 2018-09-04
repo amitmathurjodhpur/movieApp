74ee11b5b886e1b2ba729fb40470c0e3de0ab1df
amit <amathurn@in.ibm.com>
2018-09-04 11:19:58 +0530

    FIrst Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Amit Mathur on 9/4/18.
+//  Copyright © 2018 Amit Mathur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
